ab44803468a66c5cf678bd64347528e41d67c204
dalor <dalor@i.ua>
2019-12-06 10:24:25 +0200

    Some fixes

--- functions/index.js
+++ functions/index.js
@@ -2,4 +2,6 @@ const functions = require('firebase-functions');
 
 const webhook = require('./webhook')
 
-exports.webhook = functions.https.onRequest(webhook);
+exports.webhook = functions
+    .region('europe-west1')
+    .https.onRequest(webhook);
--- functions/webhook.js
+++ functions/webhook.js
@@ -2,6 +2,6 @@ const bot = require('./handlers')
 
 module.exports = (req, res) => {
     bot.handleUpdate(req.body, res).then(() => {
-        res.end('ok');
+        setTimeout(() => res.end('ok'), 1000)
     })
 }
